a4a289f0db8eab075f387ee01658f109fbbc274c
David Fiedler <d.fiedler@htlkrems.at>
2024-04-30 10:23:27 +0200

    Added movment im unity project
    Added Movment script
    added CamerarMovment script
    added actionmap + auto generatet class
    added monster ohne script und matieral
    added player object mit oritaion, player mesh , camarer
    added platfrom

---
+++ Labyrinthe Mourant/Assets/player/PlayerMovement.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Movement")]
+    public float moveSpeed;
+
+    public float groundDrag;
+
+    PlayerInput playerInput;
+    Vector3 moveVector;
+
+
+    [HideInInspector] public float walkSpeed;
+    [HideInInspector] public float sprintSpeed;
+
+    [Header("Other")]
+    public Transform orientation;
+
+    float horizontalInput;
+    float verticalInput;
+
+    Vector3 moveDirection;
+
+    Rigidbody rb;
+
+    private void Awake()
+    {
+        playerInput = new PlayerInput();
+    }
+
+    private void OnEnable()
+    {
+        playerInput.Enable();
+        playerInput.Player.Movement.performed += OnMovementPerform;
+        playerInput.Player.Movement.canceled += OnMovementCancelled;
+    }
+
+    private void OnDisable()
+    {
+        playerInput.Disable();
+        playerInput.Player.Movement.performed -= OnMovementPerform;
+        playerInput.Player.Movement.canceled -= OnMovementCancelled;
+    }
+
+    void OnMovementPerform(InputAction.CallbackContext value)
+    {
+        moveVector = value.ReadValue<Vector3>();
+    }
+
+    void OnMovementCancelled(InputAction.CallbackContext value)
+    {
+        moveVector = Vector3.zero;
+    }
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+    }
+
+    private void Update()
+    {
+        SpeedControl();
+        rb.drag = groundDrag;
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    private void MovePlayer()
+    {
+        moveDirection = moveVector.z * orientation.forward + moveVector.x * orientation.right;
+        rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+    }
+
+    private void SpeedControl()
+    {
+        Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        // limit velocity if needed
+        if (flatVel.magnitude > moveSpeed)
+        {
+            Vector3 limitedVel = flatVel.normalized * moveSpeed;
+            rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z);
+        }
+    }
+}
